d4fe84c2a752e8aa7bdf44ac159d7cda3bc4457a
Andrej Kling <boxingprofi@web.de>
2021-01-14 16:22:55 +0100

    Initial Commit

---
+++ Neuro/NeuroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroApp.swift
+//  Neuro
+//
+//  Created by Andrej Kling on 14.01.21.
+//
+
+import SwiftUI
+
+@main
+struct NeuroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
